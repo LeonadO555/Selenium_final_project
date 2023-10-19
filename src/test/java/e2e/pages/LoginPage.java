ea3ea975a6df5f791d9576b7b9ba80839495239c
Irasp <iraspjob@gmail.com>
2023-10-19 22:55:47 +0300

    preview second test

--- src/test/java/e2e/pages/LoginPage.java
+++ src/test/java/e2e/pages/LoginPage.java
@@ -1,33 +1,44 @@
-//package e2e.pages;
-//
-//import enums.AccountCredentials;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//
-//public class LoginPage extends PageBase {
-//    public LoginPage(WebDriver driver) {
-//        super(driver);
-//    }
-//    @FindBy(xpath = "//*[@placeholder='UserName']")
-//    WebElement userNameInput;
-//
-//    @FindBy(xpath = "//*[@placeholder='Password']")
-//    WebElement passwordInput;
-//
-//    @FindBy(xpath = "//*[@id='login']")
-//    WebElement loginButton;
-//
-//    public void waitForLoading() {
-////        getWait().forVisibility(loginForm);
-//        getWait().forVisibility(userNameInput);
-//        getWait().forVisibility(passwordInput);
-//        getWait().forClickable(loginButton);
-//    }
-//    public void login(AccountCredentials userName, AccountCredentials password) {
-//        userNameInput.sendKeys(userName.value);
-//        passwordInput.sendKeys(password.value);
-//        click(loginButton);
-//
-//    }
-//}
+package e2e.pages;
+
+import enums.AccountCredentials;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends PageBase {
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//*[@id='userForm']")
+    WebElement userForm;
+    @FindBy(xpath = "//*[@id='userName']")
+    WebElement userNameInput;
+    //*[@id='userForm']//*[@id='userName']
+    @FindBy(xpath = "//*[@id='userForm']//*[@id='password']")
+    WebElement passwordInput;
+    @FindBy(xpath = "//*[@id='login']")
+    WebElement loginButton;
+
+    public void waitForLoading() {
+        getWait().forVisibility(userForm);
+        getWait().forVisibility(userNameInput);
+        getWait().forVisibility(passwordInput);
+        getWait().forClickable(loginButton);
+    }
+
+    public void login(AccountCredentials userName, AccountCredentials password) {
+        loginButton.click();
+        userNameInput.sendKeys(userName.value);
+        passwordInput.sendKeys(password.value);
+        click(loginButton);
+    }
+
+        public void confirmSuccessfulLogin () {
+            getWait().forInvisibility(userForm);
+        }
+
+    public void confirmUnsuccessfulLogin() {
+        getWait().forVisibility(userForm);
+    }
+}
