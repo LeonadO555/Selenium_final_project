9e73a61b7eb26a64613911c64dc983117b8b6d34
Oygul.T <moonflowersultan@gmail.com>
2023-11-08 21:11:52 +0100

    User can log in and add book UI via Api test update

--- src/test/java/e2e/pages/ProfilePage.java
+++ src/test/java/e2e/pages/ProfilePage.java
@@ -1,19 +1,14 @@
 package e2e.pages;
 
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.time.Instant;
 
 public class ProfilePage extends PageBase{
     public ProfilePage(WebDriver driver) {
         super(driver);
     }
-    //@FindBy(xpath = "//*[@class = 'rt-tr -odd']//*[@id='delete-record-undefined']")
     @FindBy(xpath = "//span[@data-toggle='tooltip' and contains(@title, 'Delete')]/ancestor::div[contains(@class, 'action-buttons')]")
-    //@FindBy(xpath = "//*[@id = 'delete-record-undefined']")
     WebElement deleteBookButton;
     @FindBy(xpath = "//*[@class = 'modal-content']")
     WebElement modalAlert;
